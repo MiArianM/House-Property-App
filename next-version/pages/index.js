e0c7eddabfc77a034f119e0ad9ffd7fe64246a40
Arian <Arianpourhossein53@gmail.com>
2024-08-19 06:16:59 +0330

    Finished header almost

--- next-version/pages/index.js
+++ next-version/pages/index.js
@@ -1,8 +1,11 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import styles from "../styles/Hero/Hero.module.css";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination } from "swiper/modules";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import Avatar from "@mui/material/Avatar";
 export default function Home() {
   const cards = [
@@ -128,81 +131,221 @@ export default function Home() {
   ];
   return (
     <>
-      <div className="swiper-button-next swiper-navBtn"></div>
-      <div className="swiper-button-prev swiper-navBtn"></div>
-      <div className="swiper-pagination"></div>
-      <div className="slide-container swiper">
-        <Swiper
-          className="slide-content"
-          slidesPerView={5}
-          spaceBetween={1}
-          pagination={{
-            el: ".swiper-pagination",
-            clickable: true,
-            dynamicBullets: true,
-          }}
-          navigation={{
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-          }}
-          modules={[Navigation, Pagination]}
-          breakpoints={{
-            0: { slidesPerView: 1 },
-            750: { slidesPerView: 2 },
-            1100: { slidesPerView: 3 },
-            1300: { slidesPerView: 4 },
-          }}
-        >
-          {cards.map((card, index) => (
-            <SwiperSlide key={index} className="swiper-slide">
-              <div className="card">
-                <div className="image-content">
-                  <span className="overlay"></span>
-                  <div className="card-image">
+      <div className={styles.background_blur1}></div>
+      <div className={styles.background_blur2}></div>
+      <div className="container">
+        <section className="Hero">
+          <nav className={styles.Hero_nav}>
+            <div className={styles.Nav_Brand}>
+              <img src="https://i.postimg.cc/Hs9QPXDb/Brand.png" />
+              <h2 className={styles.BrandName}>-V4 | House-App</h2>
+            </div>
+            <div className={styles.ListAndUser}>
+              <ul className={styles.Nav_List}>
+                <li className={styles.Nav_item}>About</li>
+                <li className={styles.Nav_item}>Article</li>
+                <li className={styles.Nav_item}>Property</li>
+              </ul>
+              <a className={styles.Nav_User} href="github.com/MiarianM">
+                Sign Up!
+              </a>
+            </div>
+          </nav>
+          <div className={styles.Burger_Menu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={styles.Hero_content}>
+            <div className={styles.Hero_Title}>
+              <div className={styles.Title}>
+                <h3 className={styles.Title_Head}>
+                  find the place to live{" "}
+                  <span className={styles.Title_slogan}>your dreams </span>
+                  easily here
+                </h3>
+                <p className={styles.Title_paragraph}>
+                  Everything you need about finding your place to live will be
+                  here, where it will be easier for you
+                </p>
+              </div>
+              <div className={styles.Search}>
+                <img
+                  className={styles.Search_image}
+                  src="https://i.postimg.cc/XYNwMq4m/Search-Location.png"
+                />
+                <input
+                  placeholder="Search for the location you want!"
+                  className={styles.Search_input}
+                  type="text"
+                />
+                <button className={styles.Search_button}>
+                  Search &nbsp;
+                  <FontAwesomeIcon icon={faChevronRight} />
+                </button>
+              </div>
+              <div className={styles.Partnership}>
+                <span className={styles.Partnership__text}>
+                  Our Partnership
+                </span>
+                <ul className={styles.Partnership__list}>
+                  <li className={styles.Partnerships}>
                     <img
-                      src={card.imageUrl}
-                      alt="House-Image"
-                      className="card-img"
+                      className={styles.Partnerships_Image}
+                      src="https://i.postimg.cc/yd0J3C70/traveloka1.png"
                     />
-                    {card.badge && (
-                      <div className="badge">
-                        <span
-                          className={`badge_text ${card.badge
-                            .toLocaleLowerCase()
-                            .replace(" ", "_")}`}
-                        >
-                          {card.badge === "popular"
-                            ? "🔥"
-                            : card.badge === "New house"
-                            ? "🏡"
-                            : card.badge === "Best Deals"
-                            ? "💲"
-                            : "✨"}{" "}
-                          <span>&nbsp;</span>
-                          {card.badge}
-                        </span>
-                      </div>
-                    )}
-                  </div>
-                </div>
-                <div className="card-content">
-                  <h2 className="name">{card.name}</h2>
-                  <p className="description">{card.description}</p>
-                  <span className="price">$ {card.price}</span>
-                  <div className="ownerinfo">
-                    <Avatar alt="Remy Sharp" src={card.owner.pic} />
-                    <div>
-                      <h5 className="owner_name">{card.owner.name}</h5>
-                      <span className="owner_location">
-                        {card.owner.location}
-                      </span>
+                    <div className={styles.Partnerships_Details}>
+                      <a className={styles.complink} href="google.com">
+                        Details
+                      </a>
                     </div>
+                  </li>
+                  <li className={styles.Partnerships}>
+                    <img
+                      className={styles.Partnerships_Image}
+                      src="https://i.postimg.cc/Y03h8NG9/traveloka2.png"
+                    />
+                    <div className={styles.Partnerships_Details}>
+                      <a className={styles.complink} href="google.com">
+                        Details
+                      </a>
+                    </div>
+                  </li>
+                  <li className={styles.Partnerships}>
+                    <img
+                      className={styles.Partnerships_Image}
+                      src="https://i.postimg.cc/qq0N8D7S/traveloka3.png"
+                    />
+                    <div className={styles.Partnerships_Details}>
+                      <a className={styles.complink} href="google.com">
+                        Details
+                      </a>
+                    </div>
+                  </li>
+                  <li className={styles.Partnerships}>
+                    <img
+                      className={styles.Partnerships_Image}
+                      src="https://i.postimg.cc/gcqXYpSW/traveloka4.png"
+                    />
+                    <div className={styles.Partnerships_Details}>
+                      <a className={styles.complink} href="google.com">
+                        Details
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.Hero_Introduction}>
+              <img
+                className={styles.Hero_Image}
+                src="https://i.postimg.cc/Qd6cjDMt/Hero-Image.jpg"
+              />
+              <div className={styles.Hero_Services}>
+                <div className={styles.Service}>
+                  <div className={styles.Service_Customers}>
+                    <img
+                      className={styles.Customer_image}
+                      src="https://i.postimg.cc/Gpyt6n86/person3.png"
+                    />
+                    <img
+                      className={styles.Customer_image}
+                      src="https://i.postimg.cc/rwbzhwmS/person2.png"
+                    />
+                    <img
+                      className={styles.Customer_image}
+                      src="https://i.postimg.cc/XYXpcCVj/person1.png"
+                    />
+                  </div>
+                  <div className={styles.Service_information}>
+                    <h3 className={styles.information_title}>1K+ People</h3>
+                    <p className={styles.information_description}>
+                      Successfully Getting Home
+                    </p>
                   </div>
                 </div>
               </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+            </div>
+          </div>
+          <div className={`swiper_button_next ${styles.swiper_navBtn}`}></div>
+          <div className={`swiper_button_prev ${styles.swiper_navBtn}`}></div>
+          <div className={styles.swiper_pagination}></div>
+          <div className={`${styles.slide_container}`}>
+            <Swiper
+              className={styles.slide_content}
+              slidesPerView={5}
+              modules={[Navigation, Pagination]}
+              spaceBetween={1}
+              pagination={{
+                clickable: true,
+                dynamicBullets: true,
+              }}
+              navigation
+              breakpoints={{
+                0: { slidesPerView: 1 },
+                750: { slidesPerView: 2 },
+                1100: { slidesPerView: 3 },
+                1300: { slidesPerView: 4 },
+              }}
+            >
+              {cards.map((card, index) => (
+                <SwiperSlide key={index} className="swiper-slide">
+                  <div className={styles.card}>
+                    <div className={styles.image_content}>
+                      <span className={styles.overlay}></span>
+                      <div className={styles.card_image}>
+                        <img
+                          src={card.imageUrl}
+                          alt="House-Image"
+                          className={styles.card_img}
+                        />
+                        {card.badge && (
+                          <div className={styles.badge}>
+                            <span
+                              className={`badge_text ${card.badge
+                                .toLocaleLowerCase()
+                                .replace(" ", "_")}`}
+                            >
+                              {card.badge === "popular"
+                                ? "🔥"
+                                : card.badge === "New house"
+                                ? "🏡"
+                                : card.badge === "Best Deals"
+                                ? "💲"
+                                : "✨"}{" "}
+                              <span>&nbsp;</span>
+                              {card.badge}
+                            </span>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                    <div className={styles.card_content}>
+                      <h2 className={styles.name}>{card.name}</h2>
+                      <p className={styles.description}>{card.description}</p>
+                      <span className={styles.price}>$ {card.price}</span>
+                      <div className={styles.ownerinfo}>
+                        <Avatar
+                          className={styles.card_avatar}
+                          alt="owner-photo"
+                          src={card.owner.pic}
+                        />
+                        <div>
+                          <h5 className={styles.owner_name}>
+                            {card.owner.name}
+                          </h5>
+                          <span className={styles.owner_location}>
+                            {card.owner.location}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </section>
       </div>
     </>
   );
