04d9651d4839cc39b79408592b5d7a3b547089af
Arian <Arianpourhossein53@gmail.com>
2024-08-20 04:13:55 +0330

    almost finished head

--- next-version/pages/_app.js
+++ next-version/pages/_app.js
@@ -1,4 +1,17 @@
 import "../styles/global.css";
+import DefaultNav from "../Components/NavBar/DefaultNav";
+import HamburgerMenu from "../Components/NavBar/HamburgerMenu";
+import styles from "../styles/Hero/Hero.module.css";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div className="background_blur1"></div>
+      <div className="background_blur2"></div>
+      <div className="container">
+        <DefaultNav styles={styles} />
+        <HamburgerMenu styles={styles} />
+        <Component {...pageProps} />;
+      </div>
+    </>
+  );
 }
